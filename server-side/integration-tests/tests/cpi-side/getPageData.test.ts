b03ef9cae507bea22491727614f8238e555fb545
Dor Shpigel <dor.s@pepperi.com>
2024-05-20 10:18:26 +0300

    GetPageDataTest changed

--- server-side/integration-tests/tests/cpi-side/getPageData.test.ts
+++ server-side/integration-tests/tests/cpi-side/getPageData.test.ts
@@ -30,105 +30,39 @@ export class OnClientGetPageDataTest extends ABaseCpiSideTest {
     //await cpiService.sync();
 
     const page = await apiService.getPages();
-    console.log(page);
     this.pageKey = page[0].Key;
 
     const pageFromCPISide = await cpiService.emitEvent("AddonAPI", {
       AddonUUID: apiService.addonUUID,
       method: "GET",
-      RelativeURL: `/addon-cpi/get_page_data?key=${this.pageKey}`,
+      RelativeURL: `/addon-cpi/pages/${this.pageKey}`,
     });
 
     const emitEventResObj = JSON.parse(pageFromCPISide?.data?.Value);
-    console.log(emitEventResObj);
 
-    const relation = emitEventResObj?.availableBlocks[0]?.relation;
-
-    expect(emitEventResObj?.availableBlocks[0]?.addonPublicBaseURL)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(10)
-      .and.includes("Public");
-    expect(relation).to.be.an("object").that.is.not.undefined.and.is.not.null;
-    expect(relation?.AddonBaseURL)
-      .to.be.a("string")
-      .that.includes("Addon/Public");
-    expect(relation?.AddonRelativeURL)
-      .to.be.a("string")
-      .with.length.greaterThanOrEqual(5);
-    expect(relation?.AddonUUID).to.be.a("string").that.has.lengthOf(36);
-    expect(relation?.ComponentName)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(5);
-    expect(relation?.CreationDateTime).to.be.a("string").with.lengthOf(24);
-    expect(relation?.Description)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(5);
-    expect(relation?.ElementName)
-      .to.be.a("string")
-      .with.length.greaterThanOrEqual(5);
-    expect(relation?.ElementsModule)
-      .to.be.a("string")
-      .that.is.equal("WebComponents");
-    expect(relation?.Hidden).to.be.a("boolean").that.is.false;
-    expect(relation?.Key)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(10);
-    expect(relation?.ModificationDateTime)
-      .to.be.a("string")
-      .that.has.lengthOf(24);
-    expect(relation?.ModuleName)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(5);
-    expect(relation?.Name)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(0);
-    expect(relation?.RelationName).that.includes("Block");
-    expect(relation?.SubType).that.is.equal("NG14");
-    expect(relation?.Type).that.is.equal("NgComponent");
-
-    const pageView = emitEventResObj?.page;
-
-    expect(pageView.Name)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(0);
-    expect(pageView.Key).to.be.a("string").that.has.length.lessThanOrEqual(36);
-    expect(pageView.Description)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(0);
-
-    const layout = pageView?.Layout;
-
-    expect(layout.ColumnsGap)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(2);
-    expect(layout.HorizontalSpacing)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(2);
-    expect(layout.SectionsGap)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(2);
-    expect(layout.VerticalSpacing)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(2);
-    expect(layout.MaxWidth).to.be.a("number").that.is.equal(0);
-    expect(layout.HorizontalSpacing)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(2);
-    expect(layout.Sections)
-      .to.be.an("array")
-      .that.has.length.greaterThanOrEqual(0);
-
-    const block = pageView?.Blocks[0];
-
+    const block = emitEventResObj?.Blocks[0];
+  
     expect(block.Configuration).to.be.an("object").that.is.not.empty;
-    expect(block.Configuration.Data).to.be.an("object").that.is.not.empty;
     expect(block.Key).to.be.a("string").that.has.lengthOf(36);
-    expect(block.Configuration.AddonUUID)
-      .to.be.a("string")
-      .that.has.lengthOf(36);
-    expect(block.Configuration.Resource)
-      .to.be.a("string")
-      .that.has.length.greaterThanOrEqual(5);
+    expect(block.Configuration.AddonUUID).to.be.a("string").that.has.lengthOf(36);
+    expect(block.Configuration.Resource).to.be.a("string").that.has.length.greaterThanOrEqual(5);
+    expect(block.Configuration.Data).to.be.an("object").that.is.not.empty.and.does.not.deep.equal({});
+
+    const layout = emitEventResObj?.Layout;
+
+    expect(layout.ColumnsGap).to.be.a("string").that.has.length.greaterThanOrEqual(2);
+    expect(layout.HorizontalSpacing).to.be.a("string").that.has.length.greaterThanOrEqual(2);
+    expect(layout.SectionsGap).to.be.a("string").that.has.length.greaterThanOrEqual(2);
+    expect(layout.VerticalSpacing).to.be.a("string").that.has.length.greaterThanOrEqual(2);
+    expect(layout.MaxWidth).to.be.a("number").that.is.equal(0);
+    expect(layout.HorizontalSpacing).to.be.a("string").that.has.length.greaterThanOrEqual(2);
+    expect(layout.Sections).to.be.an("array").that.has.length.greaterThanOrEqual(0);
+
+    expect(emitEventResObj.Name).to.be.a("string").that.has.length.greaterThanOrEqual(0);
+    expect(emitEventResObj.Description).to.be.a("string").that.has.length.greaterThanOrEqual(0);
+    expect(emitEventResObj.Key).to.be.a("string").that.has.length.greaterThanOrEqual(36);
+
+    
 
     return new Promise((resolve, reject) => {
       resolve(true);
