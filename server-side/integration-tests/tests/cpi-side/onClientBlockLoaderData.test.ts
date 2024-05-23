8e299f27af27afc53ed68259c9b3672cf292e975
Dor Shpigel <dor.s@pepperi.com>
2024-05-23 13:28:28 +0300

    adding logs

--- server-side/integration-tests/tests/cpi-side/onClientBlockLoaderData.test.ts
+++ server-side/integration-tests/tests/cpi-side/onClientBlockLoaderData.test.ts
@@ -23,33 +23,39 @@ export class OnClientBlockLoadDataTest extends ABaseCpiSideTest {
   }
 
   async test(expect: Chai.ExpectStatic): Promise<any> {
-    const cpiService = this.pagesCPISideService();
+    try {
+      const cpiService = this.pagesCPISideService();
 
-    const eventData = { Name: "List", BlockType: "AddonBlock" };
+      const eventData = { Name: "List", BlockType: "AddonBlock" };
+  
+      const emitEvent = await cpiService.emitEvent(this.interceptor, eventData);
+      console.log(emitEvent);
+      const relation = emitEvent?.data?.relation;
+  
+      expect(emitEvent?.data?.addonPublicBaseURL).to.be.a('string').that.has.length.greaterThanOrEqual(10).and.includes('Public');
+      expect(relation).to.be.an("object").that.is.not.undefined.and.is.not.null;
+      expect(relation?.AddonBaseURL).to.be.a("string").that.includes("Addon/Public");
+      expect(relation?.AddonRelativeURL).to.be.a("string").with.length.greaterThanOrEqual(5);
+      expect(relation?.AddonUUID).to.be.a("string").that.has.lengthOf(36);
+      expect(relation?.ComponentName).to.be.a("string").that.is.equal('ListComponent');
+      expect(relation?.CreationDateTime).to.be.a("string").with.lengthOf(24);
+      expect(relation?.Description).to.be.a("string").that.has.length.greaterThanOrEqual(5);
+      expect(relation?.ElementName).to.be.a("string").with.length.greaterThanOrEqual(5).and.includes('abi');
+      expect(relation?.ElementsModule).to.be.a("string").that.is.equal("WebComponents");
+      expect(relation?.Hidden).to.be.a("boolean").that.is.false;
+      expect(relation?.Key).to.be.a("string").that.includes("List").and.includes("AddonBlock");
+      expect(relation?.ModificationDateTime).to.be.a("string").that.has.lengthOf(24);
+      expect(relation?.ModuleName).to.be.a("string").that.is.equal("ListModule");
+      expect(relation?.Name).to.be.equal("List");
+      expect(relation?.RelationName).that.is.equal("AddonBlock");
+      expect(relation?.SubType).that.is.equal("NG14");
+      expect(relation?.Type).that.is.equal("NgComponent");
+      expect(emitEvent.type).to.be.equal("Finish");
+    } catch(e) {
+      console.log('integration_tests::errorHere');
+      console.log(e);
+    }
 
-    const emitEvent = await cpiService.emitEvent(this.interceptor, eventData);
-    console.log(emitEvent);
-    const relation = emitEvent?.data?.relation;
-
-    expect(emitEvent?.data?.addonPublicBaseURL).to.be.a('string').that.has.length.greaterThanOrEqual(10).and.includes('Public');
-    expect(relation).to.be.an("object").that.is.not.undefined.and.is.not.null;
-    expect(relation?.AddonBaseURL).to.be.a("string").that.includes("Addon/Public");
-    expect(relation?.AddonRelativeURL).to.be.a("string").with.length.greaterThanOrEqual(5);
-    expect(relation?.AddonUUID).to.be.a("string").that.has.lengthOf(36);
-    expect(relation?.ComponentName).to.be.a("string").that.is.equal('ListComponent');
-    expect(relation?.CreationDateTime).to.be.a("string").with.lengthOf(24);
-    expect(relation?.Description).to.be.a("string").that.has.length.greaterThanOrEqual(5);
-    expect(relation?.ElementName).to.be.a("string").with.length.greaterThanOrEqual(5).and.includes('abi');
-    expect(relation?.ElementsModule).to.be.a("string").that.is.equal("WebComponents");
-    expect(relation?.Hidden).to.be.a("boolean").that.is.false;
-    expect(relation?.Key).to.be.a("string").that.includes("List").and.includes("AddonBlock");
-    expect(relation?.ModificationDateTime).to.be.a("string").that.has.lengthOf(24);
-    expect(relation?.ModuleName).to.be.a("string").that.is.equal("ListModule");
-    expect(relation?.Name).to.be.equal("List");
-    expect(relation?.RelationName).that.is.equal("AddonBlock");
-    expect(relation?.SubType).that.is.equal("NG14");
-    expect(relation?.Type).that.is.equal("NgComponent");
-    expect(emitEvent.type).to.be.equal("Finish");
 
 
     return new Promise((resolve, reject) => {
